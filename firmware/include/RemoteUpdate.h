6edd53bc4f115914d45a8c622f2dcebe8e2d5caf
jlc3718-source <jlc3718@gmail.com>
2026-09-09 23:21:28 -0400

    Finalize Anderson Home v2.0.5 OTA test build revision

--- firmware/include/RemoteUpdate.h
+++ firmware/include/RemoteUpdate.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <Arduino.h>
 
-// v2.0.4: signed discovery plus manually-triggered verified remote OTA install.
+// v2.0.5: signed discovery plus manually-triggered verified remote OTA install.
 String remoteUpdateStatusJson(const char* currentVersion);
 String remoteUpdateCheckJson(const char* currentVersion);
 String remoteUpdateInstallJson(const char* currentVersion);
