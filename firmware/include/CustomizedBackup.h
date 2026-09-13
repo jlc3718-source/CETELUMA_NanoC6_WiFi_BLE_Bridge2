0f1a3b1fcd4ae222be05865d847bc407e0b69b3e
jlc3718-source <jlc3718@gmail.com>
2026-09-12 22:50:06 -0400

    Finalize v3.1.12 release lineage

--- firmware/include/CustomizedBackup.h
+++ firmware/include/CustomizedBackup.h
@@ -2,6 +2,7 @@
 #include <Arduino.h>
 #include "Types.h"
 
+// Single-copy weekly backup/restore for user-customized Anderson settings.
 void customizedSettingsBackupBegin();
 bool customizedSettingsBackupCreate(const AppSettings& settings,const char* firmwareVersion,bool automatic,String& error);
 bool customizedSettingsBackupRestore(String& error);
